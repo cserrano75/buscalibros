449afb3d4c41d58f8b573afb667af940c8cb7acc
Claudio Serrano <claudio.serrano.rojas@gmail.com>
2025-06-08 17:38:35 -0400

    Genero estadisticas

--- src/main/java/com/aluracursos/buscalibros/principal/Principal.java
+++ src/main/java/com/aluracursos/buscalibros/principal/Principal.java
@@ -5,9 +5,16 @@ import com.aluracursos.buscalibros.model.DatosDeLibros;
 import com.aluracursos.buscalibros.service.ConsumoAPI;
 import com.aluracursos.buscalibros.service.ConvierteDatos;
 
+import java.util.Comparator;
+import java.util.DoubleSummaryStatistics;
+import java.util.Optional;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Principal {
     private ConsumoAPI consumoAPI = new ConsumoAPI();
     private ConvierteDatos conversor = new ConvierteDatos();
+    private Scanner teclado = new Scanner(System.in);
 
     public void muestraElMenu(){
 
@@ -17,5 +24,38 @@ public class Principal {
         var datos = conversor.obtenerDatos(json, Datos.class);
         System.out.println(datos);
 
+        //Top 10 de libros
+        System.out.println("Top 10 de libros");
+        datos.resultados().stream()
+                .sorted(Comparator.comparing(DatosDeLibros::numeroDeDescargas).reversed())
+                .map(datosDeLibros -> datosDeLibros.titulo().toUpperCase())
+                .limit(10)
+                .forEach(System.out::println);
+
+        //Busca libro por titulo o parte de el
+//        System.out.println("Escriba el titulo del libro que desea buscar: ");
+//        var parteDelTitulo = teclado.nextLine();
+//
+//        json = consumoAPI.obtenerDatos("https://gutendex.com/books?search=" + parteDelTitulo.replace(" ","+"));
+//        var datosBusqueda = conversor.obtenerDatos(json, Datos.class);
+//        Optional<DatosDeLibros> libroBuscado = datosBusqueda.resultados().stream()
+//                .filter(l-> l.titulo().toUpperCase().contains(parteDelTitulo.toUpperCase()))
+//                .findFirst();
+//
+//        if(libroBuscado.isPresent()){
+//            System.out.println("Libro encontrado!");
+//            System.out.println("Los datos son: " + libroBuscado.get());
+//        }else {
+//            System.out.println("Libro no encontrado!");
+//        }
+
+        //Trabajando con estadisticas
+        DoubleSummaryStatistics est = datos.resultados().stream()
+                .filter(datosDeLibros -> datosDeLibros.numeroDeDescargas()>0)
+                .collect(Collectors.summarizingDouble(DatosDeLibros::numeroDeDescargas));
+        System.out.println("Cantidad media de descargas: " + est.getAverage());
+        System.out.println("Cantidad maxima de descargas: " +est.getMax());
+        System.out.println("Cantidad minina de descargas: " +est.getMin());
+        System.out.println("Cantidad de registros: " +est.getCount());
     }
 }
